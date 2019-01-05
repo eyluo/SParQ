c19dc4dcda779b0ee80ec13191fbe15e86af972b
Eugene Luo <eyluo@andrew.cmu.edu>
2019-01-05 16:13:49 -0500

    created eval loop to mimic playback, reorder playlist and get track info

--- README.md
+++ README.md
@@ -2,21 +2,30 @@
 
 A Spotify party playlist.
 
-The current idea is to have a long-running process that lets users scan their 
+The current plan is to have a long-running process that lets users scan their 
 Spotify code or something equivalent to figure out popular tracks that they've 
 been listening to as well as give users the ability to influence songs by voting 
 them up or down the party queue.
+
+Currently just a concept. At the moment, not immediately feasible... try to
+imagine everyone bringing their laptops to a party. Portability will be a focus
+once a proof-of-concept has been done.
 
 ## Done:
 
 - Implemented a basic script that can generate a playlist and add songs to it 
 for an authenticated user.
+- Input loop that can control playback, give information, and reorder in a
+hard-coded manner.
 
 ## To-do:
 
+- Set up sockets so other users can vote on songs.
+- Create a buffer to add songs so they can get voted onto the queue after a set
+amount of time.
 - Use what's been done so far to systematically generate playlists based off of 
-user information
+user information.
 - Investigate audio analysis, maybe find a way to link this to beat detection in 
 a room?
 - Investigate ways to get user information. QR codes? Usernames?
-- Like a million other things
+- Like a million other things.
--- main.py
+++ main.py
@@ -3,34 +3,71 @@ import pprint
 import spotipy
 import spotipy.util as util
 
-SCOPE = 'playlist-modify-public user-read-currently-playing'
+SCOPE = 'playlist-modify-public ' + \
+        'user-read-currently-playing ' + \
+        'user-modify-playback-state'
+
+# given a Spotify object, username, and playlist name, looks for provided
+# playlist or creates a new one with the specified name.
+def fetchPlaylistID(sp, username, name):
+    playlists = sp.current_user_playlists()
+    for item in playlists['items']:
+        if (item['name'] == name):
+            return item['id']
+
+    playlist = sp.user_playlist_create(user=username,
+                                       name=name,
+                                       public=True)
+
+    pprint.pprint(sp.current_user_playlists())
+
+    return playlist['id']
 
 def main():
+    # Step 1: get user and get access token from username
     username = input('Enter username: ')
 
     token = util.prompt_for_user_token(username, SCOPE)
 
     if (token):
+        # Step 2: get playlist to modify
         sp = spotipy.Spotify(auth=token)
-        currentTrack = sp.current_user_playing_track()
-        if (currentTrack != None):
-            print('Currently playing ', currentTrack['item']['name'])
+
         name = input('Playlist name: ')
 
-        if (len(name) > 0):
-            playlist = sp.user_playlist_create(user=username,
-                                    name=name, 
-                                    public=True)
+        if (name):
+            playlistID = fetchPlaylistID(sp, username, name)
 
+        # Step 3: in a perpetual loop, modify the playlist
         while (True):
-            keyword = input('Give keyword: ')
-            result = sp.search(q=keyword, limit=1)
+            command = input('cmd: ')
+            if (command == 'i'):
+                keyword = input('Give keyword: ')
+                result = sp.search(q=keyword, limit=1)
 
-            trackID = result['tracks']['items'][0]['id']
-            
-            # sp.user_playlist_add_tracks(username, playlist['id'], [trackID])
+                trackID = result['tracks']['items'][0]['id']
+                
+                if (name): 
+                    sp.user_playlist_add_tracks(username, playlistID, [trackID])
 
-            pprint.pprint(sp.audio_analysis(trackID))
+                out = open('audio-analysis.py', 'w')
+                pprint.pprint(sp.audio_analysis(trackID), out)
+            elif (command == 's'):
+                sp.shuffle(True)
+            elif (command == 'pa'):
+                sp.pause_playback()
+            elif (command == 'pl'):
+                sp.start_playback()
+            elif (command == 'n'):
+                sp.next_track()
+            elif (command == 'pr'):
+                sp.previous_track()
+            elif (command == 'c'):
+                currentTrack = sp.current_user_playing_track()
+                if (currentTrack != None):
+                    print('Currently playing ', currentTrack['item']['name'])
+            elif (command == 'r'):
+                sp.user_playlist_reorder_tracks(username, playlistID, 3, 0)
 
         print("Done!")
     else:
